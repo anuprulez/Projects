574fe41b336504883dfa27af9097237044e481e0
anuprulez <anup.rulez@gmail.com>
2012-12-13 17:07:39 +0530

    Update src/script.js

--- src/script.js
+++ src/script.js
@@ -90,7 +90,7 @@ function writeValues(getValue){
         }
 }
 
-// evaluates the expression written in the result textbox
+// evaluates multiple operators
 function calculate(operator){
      var textBoxObject = document.getElementById('txtResult');
      var resultLength =  textBoxObject.value.length;
@@ -101,19 +101,15 @@ function calculate(operator){
      posMultiply = textBoxObject.value.indexOf("*");
      posDivide = textBoxObject.value.indexOf("/");
      posExponent = textBoxObject.value.indexOf("^");
-
      // adds two numbers
      if(posPlus > 0){
-        // takes the left part of the string i.e the first number
-        firstNumber =  textBoxObject.value.substring(0, posPlus);
-        // takes the right part of the string i.e. the second number
-        secondNumber = textBoxObject.value.substring(posPlus+1, resultLength);
-        // adds the numbers
-        if(!isNaN(parseFloat(firstNumber) + parseFloat(secondNumber))){
-             textBoxObject.value = parseFloat(firstNumber) + parseFloat(secondNumber);
-        }
+        finalOperator = '+';
+        // evaluates and output the result
+        evaluate(posPlus,finalOperator);
      }
      // subtract the two numbers
+     // there can be more than one minus operator
+     // the below code handles this situation
      else if(posMinus >=0){
         var removedObj = '';
         // if the expression starts with a negative number and followed by some other operator
@@ -149,55 +145,27 @@ function calculate(operator){
             }
         }
         else{
-            // takes the left part of the string i.e the first number
-            firstNumber =  textBoxObject.value.substring(0, posMinus);
-            // takes the right part of the string i.e. the second number
-            secondNumber = textBoxObject.value.substring(posMinus+1, resultLength);
-            // subtract the numbers
-            if(!isNaN(parseFloat(firstNumber) - parseFloat(secondNumber))){
-                 textBoxObject.value = parseFloat(firstNumber) - parseFloat(secondNumber);
-            }
+            finalOperator = '-';
+            // evaluates and output the result
+            evaluate(posMinus,finalOperator);
         }
      }
      // multiplies the two numbers
      else if(posMultiply > 0){
-        // takes the left part of string
-        firstNumber =  textBoxObject.value.substring(0, posMultiply);
-        // takes the right part of the string
-        secondNumber = textBoxObject.value.substring(posMultiply+1, resultLength);
-        // multiplies the numbers
-        if(!isNaN(parseFloat(firstNumber) * parseFloat(secondNumber))){
-              textBoxObject.value = parseFloat(firstNumber) * parseFloat(secondNumber);
-        }
+        finalOperator = '*';
+        // evaluates and output the result
+        evaluate(posMultiply,finalOperator);
      }
-     // divides the two numbers
       else if(posDivide > 0){
-        // takes the left part of the string
-        firstNumber =  textBoxObject.value.substring(0, posDivide);
-        // takes the right part of the string
-        secondNumber = textBoxObject.value.substring(posDivide+1, resultLength);
-        // divides the numbers
-        // checks if the denominator is 0 or not
-        if(parseFloat(secondNumber) > 0){
-             if(!isNaN(parseFloat(firstNumber) / parseFloat(secondNumber))){
-                 textBoxObject.value = parseFloat(firstNumber) / parseFloat(secondNumber);
-             }
-        }
-        // if the denominator is zero, result text box give 'infinite' text
-        else{
-             textBoxObject.value = "Infinite";
-        }
+        finalOperator = '/';
+        // evaluates and output the result
+        evaluate(posDivide,finalOperator);
       }
       // multiplies the two numbers
       else if(posExponent > 0){
-         // takes the left part of string
-         firstNumber =  textBoxObject.value.substring(0, posExponent);
-         // takes the right part of the string
-         secondNumber = textBoxObject.value.substring(posExponent+1, resultLength);
-         // multiplies the numbers
-         if(!isNaN(parseFloat(firstNumber) ^ parseFloat(secondNumber))){
-              textBoxObject.value = Math.pow(parseFloat(firstNumber),parseFloat(secondNumber));
-         }
+         finalOperator = '^';
+         // evaluates and output the result
+         evaluate(posExponent,finalOperator);
       }
       // appends the operator to the end of evaluated expression
       // also avoids the duplication of the mathematical operators
@@ -210,186 +178,34 @@ function calculate(operator){
          }
       }
 }
-
-///////////// Calculator code ends /////////////////////////////////////////////////////////////////////////////////
-
-//////////////////// Practice code starts //////////////////////////////////////////////////////////////////////////
-
-/*function sample(){
-    /*var book = {
-    name: 'Catch-22',
-    published: 1961,
-    doWork: function(){
-      alert('a function defined');
-    }
-    author: {
-        firstname: 'Joseph',
-        lastname: 'Heller',
-    }
-    };*/
-
-    /*function Vehicle(name){
-        this.Name = name;
-        this.Color = 'Red';
-        this.driveHow: function(){
-            alert('The vehicle ' + this.Name + ' not drivable');
-        }
-    } */
-
-     // defines a class
-     /*function Hero(name) {
-        this.name = name;
-        this.occupation = 'Ninja';
-        this.whoAreYou = function() {
-            alert("I'm " + this.name + " and I'm a " + this.occupation);
+// actually evaluates the expression and output the result
+function evaluate(posOperator,operatorType){
+     var textBoxObject = document.getElementById('txtResult');
+     var resultLength =  textBoxObject.value.length;
+     // takes the left part of the string i.e the first number
+     firstNumber =  textBoxObject.value.substring(0, posOperator);
+     // takes the right part of the string i.e. the second number
+     secondNumber = textBoxObject.value.substring(posOperator+1, resultLength);
+     // divides the numbers
+     if(operatorType == '/'){
+          if(parseFloat(secondNumber) > 0){
+               if(!isNaN(eval(parseFloat(firstNumber) + operatorType + parseFloat(secondNumber)))){
+                   textBoxObject.value = eval(parseFloat(firstNumber) + operatorType + parseFloat(secondNumber));
+               }
+          }
+          // if the denominator is zero, result text box give 'infinite' text
+          else{
+               textBoxObject.value = "Infinite";
+          }
      }
+     // evaluates the exponent operator
+     else if(operatorType == '^'){
+        textBoxObject.value = Math.pow(parseFloat(firstNumber),parseFloat(secondNumber));
      }
-     var h1 = new Hero('Michelangelo');
-     var h2 = new Hero('Donatello');
-     h1.whoAreYou();   */
-     //console.log(h2.name);
-     //console.log(h2.occupation);
-     //var vehicle1 = new Vehicle('Honda');
-     //vehicle1.driveHow();
-     //alert(book.author.doWork());
-     //alert(animal.legs.fingers);
-
-     /*function Person(name){
-        // private variable and function
-        var age = 100;
-        // private function
-        function howtowork(){age = age - 1; alert(age);}
-        // privileged function
-        this.privilegeWork = function(){
-            howtowork();
-            if (age >= 100)  {alert('Age greater than and equal to 100');}
-            else{alert('Age is less than 100');}
-            //return true;
-        }
-        // public properties
-        this.Name = name;
-
-        // public methods
-        Person.prototype.socialize = function(){alert('Public method and legs: ' + this.hasLegs); howtowork();}
-
-        // prototype properties
-        Person.prototype.hasLegs = true;
-
-        // static properties
-        Person.staticLegs = false;
+     // evaluates the +, - and * operators
+     else{
+         if(!isNaN(eval(parseFloat(firstNumber) + operatorType +  parseFloat(secondNumber)))){
+              textBoxObject.value = eval(parseFloat(firstNumber) + operatorType + parseFloat(secondNumber)) ;
+         }
      }
-      var person1 = new Person('myname');
-
-      //person1.privilegeWork();
-      //person1.socialize();
-      //alert(person1.hasLegs);
-      //for(var i in person1){
-        //console.log('property: ' + i + ' in ' + person1[i] );
-      //}
-
-      //alert('Checks for the prototype property: ' + person1.hasLegs.isPrototypeOf())
-      // Inheritance from a parent class to child class
-      // Changing the prototype of child class of parent type
-      Suan.prototype = new Person('Susan');
-      // changing the prototupe constructor of the child class of child class type
-      Suan.prototype.constructor = Suan;
-      Suan.prototype.baseClass = Person.prototype.constructor;
-      // definition of child class
-      function Suan(name){
-        var myOffice = 'ABX Company';
-        this.Name = name;
-        // public function
-        this.myFunction = function(){
-            alert('In child class now');
-        }
-        this.privilegeWork = simpleHelp;
-
-        function simpleHelp(){
-            alert('Inside simple help and my ofc is: '+ myOffice);
-        }
-      }
-      // creating the child class object
-      var Suan1 = new Suan('Susan2');
-      // calling the parent class method - the inherited method
-      //Suan1.socialize();
-      //alert(Suan.prototype.baseClass);
-
-      //Suan1.helpMe();
-
-
-      // Polymorphism - baseClass and child class have the same method name with different
-      // implementation
-      //Suan1.privilegeWork();
-      //person1.privilegeWork();
-
-      // calling the child class method
-      //Suan1.myFunction();
-      //alert(Suan.prototype.constructor);
-      //Suan1.howtowork();
-     // function that uses the Person class
-     //function usePersonClass(){
-     //}
-
-     // base class
-     function Area(shapeName){
-        this.shape = shapeName;
-        this.getArea = getArea;
-        var areaMessage = 'Area getting calculated';
-        //public method of base class
-        function getArea(){
-            alert(areaMessage);
-            alert('Area of the '+ this.shape +' is: ' + this.area);
-        }
-        // privileged method of base class
-        Area.prototype.getMessage = function(){
-            alert('Inside the privileged method of base class');
-        }
-     }
-     // child class inheritance
-     //Circle.prototype = new Area('circle');
-     //Circle.prototype.constructor = Circle;
-     // assigning the base class constructor to child class
-     //Circle.prototype.baseClass = Area.prototype.constructor;
-     // child class declaration
-     function Circle(radius){
-        //this.area = 3.14 * (Math.pow(radius,2));
-        this.len = 5;
-        this.rad = this.rad + 1;
-        alert('The radius of the circle is '+ this.rad + 'and the length is fine: '+ this.len);
-     }
-
-     // creating first object of child class
-     //var circleArea = new Circle(10);
-     //circleArea.getArea();
-     //circleArea.getMessage();
-
-     // Usage of Call and Apply method
-     var circle1 = new Object();
-     circle1.rad = 20;
-     //Circle.call(circle1, 20);
-     //Circle.apply(circle1, [20]);
-
-     // Closures in JavaScript
-     // Objects are alive even after the function has returned
-     function getInformation(info){
-        this.getMessage = 'Final Message';
-
-        alert(this.getMessage + ' and Closures are defined');
-        return info;
-     }
-     var saveInformation = getInformation('All Information');
-     saveInformation();
-
-
-     /*window.onError = function(){
-             alert('error occurred');
-     }
-
-     var newErrorFunction = function(){
-        alert('alert error');
-     }
-     newErrorFunction();
-     //document.write('sdfsdf
-     //alert('Get some errors'  */
-//}
-////////////////////////////////////// Practice code ends ////////////////////////////////////////////////////////
+}
